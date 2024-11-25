ec25b0f91f22c2891dd0afa03e51b311b8a4c6ab
Charles C <66725307+CharlesPlusC@users.noreply.github.com>
2024-11-25 14:08:55 +0000

    unignore JB08 inputs

--- source/tools/orekit_tools.py
+++ source/tools/orekit_tools.py
@@ -1,9 +1,9 @@
 import orekit
-from orekit.pyhelpers import setup_orekit_curdir, datetime_to_absolutedate
+from orekit.pyhelpers import setup_orekit_curdir, datetime_to_absolutedate,download_orekit_data_curdir
 
-# orekit.pyhelpers.download_orekit_data_curdir()
+download_orekit_data_curdir()
+setup_orekit_curdir()
 vm = orekit.initVM()
-setup_orekit_curdir("misc/orekit-data.zip")
 
 from orekit.pyhelpers import datetime_to_absolutedate
 from org.hipparchus.geometry.euclidean.threed import Vector3D
