32c5e34d7fc50a229b13a44a024eae3609ad8eb4
Charles C <66725307+CharlesPlusC@users.noreply.github.com>
2024-11-25 11:33:31 +0000

    reduce memory to reduce Q time for debugging

--- source/DensityInversion/StormTimeEDRDensity.py
+++ source/DensityInversion/StormTimeEDRDensity.py
@@ -78,7 +78,7 @@ def create_and_submit_density_jobs():
         # Create job script
         script_filename = os.path.join(folder_for_jobs, f"{spacecraft}_density_inversion.sh")
         script_content = f"""#!/bin/bash -l
-#$ -l h_rt=24:0:0
+#$ -l h_rt=0:10:0
 #$ -l mem=4G
 #$ -N {spacecraft}_density_inversion
 #$ -t 1-{len(storm_files)}
